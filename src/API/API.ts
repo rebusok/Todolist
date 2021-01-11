ee1646fdcf438aea05b93820b335eb6e9b858243
Yuriy <rebusok@yandex.ru>
2021-01-11 19:30:08 +0300

    fin 11

---
+++ src/API/API.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const key = "be583272-b0d8-4135-8f53-6b8fcf5092e2";
+const configOMB = {
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    withCredentials:true,
+    headers:{
+        "API-KEY": key
+    }
+};
+
+const axiosInstance = axios.create(configOMB);
+
+const API = {
+
+}
