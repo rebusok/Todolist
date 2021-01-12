da82f7ee049d55e7a323e436605fce971186f2e3
Yuriy <rebusok@yandex.ru>
2021-01-12 03:33:00 +0300

    fin 12

--- src/API/API.ts
+++ src/API/API.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const key = "be583272-b0d8-4135-8f53-6b8fcf5092e2";
 const configOMB = {
-    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials:true,
     headers:{
         "API-KEY": key
@@ -12,5 +12,8 @@ const configOMB = {
 const axiosInstance = axios.create(configOMB);
 
 const API = {
+    getTodoList: () => {
+        return axiosInstance.get('todo-lists')
+    },
 
 }
