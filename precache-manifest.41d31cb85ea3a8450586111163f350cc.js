786a883d824dbb53a7cbf8ff2f69ce2bcb7ddf27
Yuriy <rebusok@yandex.ru>
2021-03-04 17:43:20 +0300

    Updates

---
+++ precache-manifest.41d31cb85ea3a8450586111163f350cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "271585a086f8099440f3cc56b2456b16",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "dd30a4bf984f3b0e8336",
+    "url": "/Todolist/static/css/main.5a142208.chunk.css"
+  },
+  {
+    "revision": "047646c6104a2ca4c2fc",
+    "url": "/Todolist/static/js/2.9add0078.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Todolist/static/js/2.9add0078.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd30a4bf984f3b0e8336",
+    "url": "/Todolist/static/js/main.ebad32fa.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
